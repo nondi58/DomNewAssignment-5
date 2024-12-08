1774e72ef4de4f3f821caf5a456fc7e3c3543585
nondi58 <ntalukder190@gmail.com>
2024-12-08 18:14:54 +0600

    third commit

--- js/donation.js
+++ js/donation.js
@@ -4,12 +4,25 @@ calculateButton.addEventListener('click',function(){
 
 
     const donationInput=parseFloat(document.getElementById('donation-amount').value);
+    // const totalAmount=document.getElementById('total-donate').innerText
    
    const totalAmount=parseFloat(document.getElementById('total-donate').innerText) ;
+   console.log(totalAmount);
   
    const newBallance= donationInput + totalAmount;
    console.log(newBallance);
    document.getElementById('total-donate').innerText = newBallance ;
+   
+
+
+   const totalDivide=parseFloat(document.getElementById('total-divided').innerText) ;
+   const newDivideBallance=  totalDivide -donationInput;
+  
+   document.getElementById('total-divided').innerText = newDivideBallance ;
+  
+
+   
+
   
 
 });
