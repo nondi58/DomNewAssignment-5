b26a977a6f562c3401d67c9eeb914124d5c260e4
nondi58 <ntalukder190@gmail.com>
2024-12-08 18:37:54 +0600

    comiit

--- js/donation.js
+++ js/donation.js
@@ -2,28 +2,23 @@ console.log("connected");
 const calculateButton=document.getElementById('donate-button');
 calculateButton.addEventListener('click',function(){
 
-
-    const donationInput=parseFloat(document.getElementById('donation-amount').value);
-    // const totalAmount=document.getElementById('total-donate').innerText
-   
-   const totalAmount=parseFloat(document.getElementById('total-donate').innerText) ;
+    const donationInput=parseFloat(document.getElementById('donation-amount').value); 
+if (!isNaN(donationInput) && donationInput > 0) {
+      const totalAmount=parseFloat(document.getElementById('total-donate').innerText) ;
    console.log(totalAmount);
-  
+//   add
    const newBallance= donationInput + totalAmount;
    console.log(newBallance);
-   document.getElementById('total-donate').innerText = newBallance ;
-   
+   document.getElementById('total-donate').innerText = newBallance ;  
 
 
+// divide
    const totalDivide=parseFloat(document.getElementById('total-divided').innerText) ;
    const newDivideBallance=  totalDivide -donationInput;
-  
    document.getElementById('total-divided').innerText = newDivideBallance ;
-  
-
+document.getElementById('donation-amount').value = "";
+} else {
+    alert("Please enter a valid donation amount.");
+}
    
-
-  
-
 });
-
